f74d2e7039c90c11f5282d8a78c83152e3ea14f4
cesarjrr2403-dot <cesarjrr2403@gmail.com>
2026-02-24 14:21:04 -0500

    commit 9f8e7d6c5b4a3e2f1d0c9b8a7e6f5d4c3b2a1 Author: Your Name <cesarjrr2403@gmail.com> Date:   Fri Jun 7 12:34:56 2024 -0400
        Agrega archivos de Python y elimina el archivo de Bases y funcionamiento

---
+++ PYTHON/00_Inicio_basico.py
@@ -0,0 +1,37 @@
+# Print:nos ayuda a imprimir lo que este dentro de los parentesis, es decir nos ayuda a mostrar algo en pantalla, 
+# puede ser un texto, un numero o una operacion matematica
+
+
+print ("Cesar Rangel")
+print('hola python')
+
+"""
+Otra manera de comentar algo en python se usa con 3  comillas dobles o simples, esto se llama docstring 
+y se usa para documentar el codigo,
+esto es muy util si quieres cometar y docuemntar un codigo en una o mas lineas,
+es decir comentario en varias lineas
+"""
+
+# NUMEROS o o interpretacion numerica en codigo,encontramos 3 tipos de numeros en python:
+
+'''
+integer
+float
+complex
+'''
+#integer: son numeros enteros, es decir sin decimales, pueden ser positivos o negativos
+#float: son numeros con decimales, pueden ser positivos o negativos
+#complex: son numeros complejos, es decir tienen una parte real y una parte imaginaria
+
+#type:
+''''
+Esto permite conocer que tipo de dato estamos usando en nuestro codigo y nos ayuda a identificar 
+si estamos usando el tipo de dato correcto para nuestra variable o operacion matematica,
+es decir nos ayuda a identificar el tipo de dato que estamos usando en nuestro codigo
+'''
+print(type(10)) #integer
+print(type(3.14)) #float
+print(type(1+2j)) #complex
+print(type("hola python")) #string
+print(type("True"))
+      
